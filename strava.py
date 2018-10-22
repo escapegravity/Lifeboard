fcda1d937e7eaa2bfe80cee4807c1d350f358bd2
Joakim Olsson <joakim.olsson90@gmail.com>
2018-10-22 19:06:15 +0200

    Added gitignore and refactored Strava.py

--- strava.py
+++ strava.py
@@ -4,19 +4,20 @@ import json
 domain = 'https://www.strava.com'
 baseApiUrl = '/api/v3'
 
+with open('config.json') as json_data_file:
+    jsonData = json.load(json_data_file)
+    clientId = jsonData['clientId']
+    clientSecret = jsonData['clientSecret']
+    refreshToken = jsonData['refreshToken']
+
+
 def RefreshToken(): 
-    clientId = '29492'
-    clientSecret = 'af5bdd207222b44859bd7172c036f0f6a1cc8b17'
-    refreshToken = 'c071f4869ae855c1b329adc9a6a49bb5f37aec90 '
-
     response = requests.post('https://www.strava.com/oauth/token', data='client_id={}&client_secret={}&grant_type=refresh_token&refresh_token={}'.format(clientId, clientSecret, refreshToken))
-
     return response.json()['access_token']
 
 def GetFromStravaAPI(endpoint):
     access_token = RefreshToken()
     response = requests.get('{}{}/{}'.format(domain, baseApiUrl, endpoint), headers={'Authorization': 'Bearer {}'.format(access_token)})
-
     return response.json()
 
 def GetAthleteInfo():
